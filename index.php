a09d79c728d8a94edc8c5074ce570d0c605a0be1
Dominik Ivković <73838697+divkovic1@users.noreply.github.com>
2021-04-19 20:17:14 +0200

    Index plus view

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 //echo __DIR__;
 define('BP',__DIR__ . DIRECTORY_SEPARATOR);
-define('BP_APP',__DIR__ . DIRECTORY_SEPARATOR);
+define('BP_APP',__DIR__ . DIRECTORY_SEPARATOR .'app' .DIRECTORY_SEPARATOR);
 //echo BP;
 
 $path=implode(
@@ -22,6 +22,7 @@ $path=implode(
     spl_autoload_register(function($class){
         $path = explode(PATH_SEPARATOR,get_include_path());
         foreach ($path as $p){
+            
             if (file_exists($p . DIRECTORY_SEPARATOR . $class . '.php')){
                 include $p . DIRECTORY_SEPARATOR . $class . '.php';
             }
