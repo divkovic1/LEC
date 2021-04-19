a9345370be69c0ccd39bdd666537050617321ebc
Dominik Ivković <73838697+divkovic1@users.noreply.github.com>
2021-04-19 18:17:19 +0200

    autoload

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
 
-echo '<pre>';
-print_r($SERVER);
-echo '</pre>';
+session_start();
+
+//echo __DIR__;
+define('BP',__DIR__ . DIRECTORY_SEPARATOR);
+
+//echo BP;
+
+$path=implode(
+    PATH_SEPARATOR,
+    [
+        BP . 'model',
+        BP . 'controller'
+
+    ]
+
+);
+
+    set_include_path ($path);
+
+    spl_autoload_register(function($class){
+        $path = explode(PATH_SEPARATOR,get_include_path());
+        foreach ($path as $p){
+            if (file_exists($p . DIRECTORY_SEPARATOR . $class . '.php')){
+                include $p . DIRECTORY_SEPARATOR . $class . '.php';
+            }
+        }
+    });
+
+    App::start();
 
 ?>
