5bad6f707c4e0d20e763ddd4353e2b5d6bcd18fc
Dominik Ivković <73838697+divkovic1@users.noreply.github.com>
2021-05-26 13:38:04 +0200

    Production preparation

--- app/config.php
+++ app/config.php
@@ -1,13 +1,24 @@
 <?php
 
-return [
-    'url'=>'http://polaznik26.edunova.hr/',
-    'AppName'=>'LEC Overview',
-    'base'=>[
+$dev=$_SERVER['REMOTE_ADDR']==='127.0.0.1' ? true : false;
+if($dev){
+    $database=[
         'server'=>'localhost',
-        'database'=>'lec',
+        'database'=>'edunovapp22',
         'user'=>'edunova',
         'password'=>'edunova'
-    ]
-
-];
+        ]
+    ];  
+}else{
+    $base=[
+        'server'=>'localhost',
+        'database'=>'xxxxx',
+        'user'=>'xxxxx',
+        'password'=>'xxxxx'
+    ];
+}
+return [
+    'url'=>'http://polaznik26.edunova.hr/',
+    'AppName'=>'LEC'
+    'database'=>$database
+]
