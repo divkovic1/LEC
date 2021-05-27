cec3dd1a98c7dc930ed11e59e7b595911256d309
Dominik Ivković <73838697+divkovic1@users.noreply.github.com>
2021-05-27 14:12:38 +0200

    CRUD and name control for 'player'

--- app/controller/PlayerController.php
+++ app/controller/PlayerController.php
@@ -14,4 +14,32 @@ class PlayerController extends AuthorizationController
             'players'=>Player::loadEverything()
         ]);
     }
+
+    public function new()
+    {
+        if($_SERVER['REQUEST_METHOD']==='GET'){
+            $player = new stdClass();
+            $player->name='';
+            $player->surname='';
+            $player->country='';
+            $player->nickname='';
+            $player->lane='';
+            $this->view->render($this->viewDir . 'new',[
+                'player'=>$player,
+                'message'=>'Enter the details please'
+            ]);
+            return;
+        }
+
+
+        $player = (object) $_POST;
+
+        if(strlen(trim($player->nickname))===0){
+            $this->view->render($this->viewDir . 'new',[
+                'player'=>$player,
+                'message'=>'The nickname is required.'
+            ]);
+            return;
+        }
+    }
 }
