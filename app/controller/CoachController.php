75cc2c305dc2ca17acdabac9aff267fa36de4941
Dominik Ivković <73838697+divkovic1@users.noreply.github.com>
2021-06-22 12:40:46 +0200

    Adding a new coach prep

--- app/controller/CoachController.php
+++ app/controller/CoachController.php
@@ -17,4 +17,35 @@ class CoachController extends AuthorizationController
             'entities'=>Coach::loadEverything()
         ]);
     }
+    public function new()
+    {
+        if($_SERVER['REQUEST_METHOD']==='GET'){
+            $this->newEntity();
+            return;
+        }
+        $this->entity = (object) $_POST;
+
+        Coach::addNew($this->entity);
+        $this->index();
+    }
+
+    private function newEntity()
+    {
+        $this->entity = new stdClass();
+        $this->entity->name='';
+        $this->entity->surname='';
+        $this->entity->nickname='';
+        $this->entity->country='';
+        $this->message='Please enter the details';
+        $this->newView();
+    }
+
+    private function newView()
+    {
+        $this->view->render($this->viewDir . 'new',[
+            'entity'=>$this->entity,
+            'message'=>$this->message
+        ]);
+    }
+
 }
--- app/model/Coach.php
+++ app/model/Coach.php
@@ -17,4 +17,25 @@ class Coach
         $expression->execute();
         return $expression->fetchAll();
     }
+
+
+    public static function addNew($entity)
+    {
+        $connection = DB::getInstance();
+        $connection->beginTransaction();
+        $expression=$connection->prepare('
+        
+            insert into coach
+            (name,surname,nickname,country) values
+            (:name, :surname, :nickname, :country)
+        
+        ');
+        $expression->execute([
+            'name'=>$entity->name,
+            'surname'=>$entity->surname,
+            'nickname'=>$entity->nickname,
+            'country'=>$entity->country
+        ]);
+        $connection->commit();
+    }
 }
